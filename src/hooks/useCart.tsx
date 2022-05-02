7eeaf5e69f7b49e042543491500ea0e289ed1440
Yvan Brito <yvanbrito96@gmail.com>
2022-05-02 12:38:46 -0300

    Teste
    outro teste

--- src/hooks/useCart.tsx
+++ src/hooks/useCart.tsx
@@ -1,7 +1,13 @@
-import { createContext, ReactNode, useContext, useState } from 'react';
-import { toast } from 'react-toastify';
-import { api } from '../services/api';
-import { Product, Stock } from '../types';
+import {
+  createContext,
+  ReactNode,
+  useContext,
+  useEffect,
+  useState,
+} from "react";
+import { toast } from "react-toastify";
+import { api } from "../services/api";
+import { Product, Stock } from "../types";
 
 interface CartProviderProps {
   children: ReactNode;
@@ -34,9 +40,25 @@ export function CartProvider({ children }: CartProviderProps): JSX.Element {
 
   const addProduct = async (productId: number) => {
     try {
-      // TODO
-    } catch {
-      // TODO
+      const index = cart.findIndex((p) => p.id === productId);
+      if (index >= 0) {
+        const { data: stock } = await api.get<UpdateProductAmount>(
+          `/stock/${productId}`
+        );
+        if (cart[index].amount >= stock.amount) throw Error("Fora do estoque");
+
+        const newCart: Product[] = [...cart];
+        newCart[index].amount += 1;
+        setCart([...newCart]);
+        return;
+      }
+      const { data: product } = await api.get<Product>(
+        `/products/${productId}`
+      );
+
+      setCart((oldState) => [...oldState, { ...product, amount: 1 }]);
+    } catch (error: any) {
+      toast.error(error.message);
     }
   };
 
